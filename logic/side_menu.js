544fd9805553ebed815193b76c2e9b535bb6e939
Yudai-creator <yudaipx77@gmail.com>
2022-09-21 15:22:27 -0400

    changes

--- logic/side_menu.js
+++ logic/side_menu.js
@@ -1,6 +1,10 @@
 let arrow = document.querySelector(".arrow_function");
 let sideMenuContainer = document.querySelector(".side_menu");
 
+let supplierListContainer = document.querySelector(".supplier");
+let profileInfoContainer = document.querySelector(".profile");
+let homeContainer = document.querySelector(".home");
+
 let routes = document.querySelectorAll(".button_route");
 let routeText = document.querySelectorAll(".route_text");
 
@@ -11,18 +15,37 @@ let flag = true;
 arrow.addEventListener("click", () => {
     if(flag){
         arrow.style.transform = "rotate(180deg)";
-        arrow.style.left = "4%"
+        arrow.style.left = "4%";
 
-        sideMenuContainer.style.width = "5%"
+        sideMenuContainer.style.width = "5%";
+
+        if(homeContainer){
+            homeContainer.style.width = "95%";
+        }
+        
+        if(profileInfoContainer){
+            profileInfoContainer.style.width = "95%";
+        }
+        
+
 
         for (let i = 0; i < routeText.length; i++) {
             routeText[i].style.display = "none";
         }
     }else{
         arrow.style.transform = "rotate(0deg)";
-        arrow.style.left = "13.8%"
+        arrow.style.left = "13.8%";
 
         sideMenuContainer.style.width = "15%";
+
+        if(homeContainer){
+            homeContainer.style.width = "85%";
+        }
+        
+        if(profileInfoContainer){
+            profileInfoContainer.style.width = "85%";
+        }
+        
 
         for (let i = 0; i < routeText.length; i++) {
             routeText[i].style.display = "block";
--- logic/supplier_profile.js
+++ logic/supplier_profile.js
@@ -1,26 +1,42 @@
 let bodyInfo = document.querySelector(".body_info");
 let productsTab = document.querySelector(".products");
+let certificationsTab = document.querySelector(".certifications")
 let companyDetails = document.querySelector(".company_details");
 
 
-let tabAction = document.querySelector(".tab_action");
+let tabAction = document.querySelectorAll(".tab_action");
 
 let overview = document.querySelector("#overview");
 
-tabAction.addEventListener("click", () => {
-    
-    bodyInfo.style.display = "none"
-    companyDetails.style.display = "none"
+let isClicked = true;
 
-    productsTab.style.display = "block"
-       
-})
+for (let i = 0; i < tabAction.length; i++) {
+    tabAction[0].addEventListener("click", () => {
+        productsTab.style.display = "none";
+        certificationsTab.style.display= "none";
 
-overview.addEventListener("click", () => {
-    bodyInfo.style.display = "block"
-    companyDetails.style.display = "block"
+        bodyInfo.style.display = "block";
+        companyDetails.style.display = "block";
+    })
 
-    productsTab.style.display = "none"
-})
+    tabAction[1].addEventListener("click", () => {
+        bodyInfo.style.display = "none";
+        companyDetails.style.display = "none";
+        certificationsTab.style.display= "none";
+
+        productsTab.style.display = "block";
+    })
+
+    tabAction[2].addEventListener("click", () => {
+        bodyInfo.style.display = "none";
+        companyDetails.style.display = "none";
+
+        productsTab.style.display = "none";
+
+        certificationsTab.style.display= "block";
+    })
+}
+
+
 
 
